41d2bf398318271ace32205cfb7a3d1e53a3c1af
Rebekah Wise <RebekahWise@MacBook-Pro.local>
2024-10-12 19:05:12 -0400

    jQuery Ex

--- jQueryEx/jqueryex.js
+++ jQueryEx/jqueryex.js
@@ -18,9 +18,39 @@ $("title").css("font-size", "50px");
 
 //5. Add an item to the list; it can say whatever you want.
 
-$("ol").append(li.innerText("list item"));
+$("ol").append($("<li>Another list item</li>"));
 
 //6. Scratch that; the list is silly. Empty the aside and put a paragraph in it apologizing for the list’s existence.
 
+$("aside")
+  .empty()
+  .append(
+    "<p>Sorry that lists suck and there was a list here before. I have now removed the list because noone really likes lists</p>"
+  );
+
 //7. When you change the numbers in the three inputs on the bottom, the background color of the body should change to match whatever the three values in lhe inputs are.
-//8. Add an event listener so that when you click on the image, it is removed from the DOM.
+$(".form-control").on("keyup blur change", function () {
+  let red = $(".form-control").eq(0).val();
+  let blue = $(".form-control").eq(1).val();
+  let green = $(".form-control").eq(2).val();
+  $("body").css(
+    "background-color",
+    "rgb(" + red + "," + green + "," + blue + ")"
+  );
+});
+
+// 8. Add an event listener so that when you click on the image,
+//    it is removed from the DOM.
+
+$("img").on("click", function (e) {
+  $(e.target).remove();
+});
+
+//Movies App
+// - Contains a form with two inputs for a title and rating along with a button to submit the form.
+
+// - When the form is submitted, capture the values for each of the inputs and append them to the DOM along with a button to remove each title and rating from the DOM.
+// - When the button to remove is clicked, remove each title and rating from the DOM.
+// - Ensure that the rating of a movie can only be between 0 and 10.
+// - Ensure that a title has at least 2 characters in it.
+// - Allow users to sort alphabetically by the title of the movie or by the rating of the movie from lowest to highest and vice versa.
