5a777c32a5ef5a934d4ce2b0c2ebddd2bb2eba64
Rebekah Wise <RebekahWise@MacBook-Pro.local>
2024-12-17 23:44:03 -0500

    Python Exercises

---
+++ python-ds-practice/14_compact/compact.py
@@ -0,0 +1,11 @@
+def compact(lst):
+    """Return a copy of lst with non-true elements removed.
+
+        >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
+        [1, 2, 'All done']
+    """
+    truthy = []
+    for item in lst:
+        if item:
+            truthy.append(item)
+    print(truthy)
