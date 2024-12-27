5dfe0038af85e36e8381c6a4cf884585de3ebf27
Rebekah Wise <RebekahWise@MacBook-Pro.local>
2024-12-26 22:16:00 -0500

    TV show API

--- apis-tvmaze/tvmaze.js
+++ apis-tvmaze/tvmaze.js
@@ -4,7 +4,6 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
-
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
@@ -12,42 +11,48 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+async function getShowsByTerm(searchTerm) {
+  let response = await axios.get(
+    `http://api.tvmaze.com/search/shows?q=<${searchTerm}>`
+  );
+  let results = [];
+  let showImage = "";
+  console.log(response);
+  for (let i = 0; i < response.data.length; i++) {
+    let showId = response.data[i].show.id;
+    let showName = response.data[i].show.name;
+    let showSummary = response.data[i].show.summary;
+    try {
+      showImage = response.data[i].show.image.original;
+    } catch (err) {
+      showImage =
+        "https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg";
     }
-  ];
+    console.log(showId);
+    console.log(showImage);
+    results.push({
+      id: showId,
+      name: showName,
+      summary: showSummary,
+      image: showImage,
+    });
+  }
+  console.log(results);
+  return results;
 }
-
 
 /** Given list of shows, create markup for each and to DOM */
 
 function populateShows(shows) {
   $showsList.empty();
-
+  console.log(shows);
   for (let show of shows) {
     const $show = $(
       `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src=${show.image}
+              alt="https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -58,12 +63,12 @@ function populateShows(shows) {
            </div>
          </div>
        </div>
-      `);
+      `
+    );
 
     $showsList.append($show);
   }
 }
-
 
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
@@ -82,13 +87,45 @@ $searchForm.on("submit", async function (evt) {
   await searchForShowAndDisplay();
 });
 
-
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+  const response = await axios.get(
+    `http://api.tvmaze.com/shows/<${id}>/episodes`
+  );
+  return response.data.map((e) => ({
+    id: e.id,
+    name: e.name,
+    season: e.season,
+    number: e.number,
+  }));
+}
 
 /** Write a clear docstring for this function... */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) {
+  $episodesList.empty();
+
+  for (let episode of episodes) {
+    const $item = $(
+      `<li>
+         ${episode.name}
+         (season ${episode.season}, episode ${episode.number})
+       </li>
+      `
+    );
+
+    $episodesList.append($item);
+  }
+
+  $episodesArea.show();
+}
+async function getEpisodesAndDisplay(evt) {
+  const showId = $(evt.target).closest("[data-show-id]").data("show-id");
+  const episodes = await getEpisodesOfShow(showId);
+  populateEpisodes(episodes);
+}
+
+$showsList.on("click", ".Show-getEpisodes", getEpisodesAndDisplay);
--- python-ds-practice/11_flip_case/flip_case.py
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -9,10 +9,11 @@ def flip_case(phrase, to_swap):
 
         >>> flip_case('Aaaahhh', 'h')
         'AaaaHHH'
-
-    """
+        """
     lst = list(phrase)
+    swapped = ''
     for letter in lst:
-        if letter.lower() == to_swap:
-            letter.swapcase()
-    print(''.join(lst))
+        if letter.lower() == to_swap.lower():
+           swapped = swapped + letter.swapcase()
+        else: swapped = swapped + letter
+    print(swapped)
